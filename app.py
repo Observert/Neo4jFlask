175b692ce9b424e5c7f3e45822c8cd5a94b6348d
Observert <yash0702@gmail.com>
2019-04-13 22:51:33 +0530

    Modifying requirements to cater for vulnerabilities.

--- app.py
+++ app.py
@@ -4,61 +4,59 @@ import config
 import business_center
 import role
 import people
-from flask_cors import CORS, cross_origin
 
 uri = config.getNeo4jUri()
 driver = GraphDatabase.driver(uri, auth=(config.username, config.password))
 
 app = Flask(__name__)
-CORS(app, support_credentials=True)
 
-@app.route('/',methods=['GET'])
-@cross_origin(supports_credentials=True)
+
+@app.route('/', methods=['GET'])
 def index():
     return "Success"
 
-@app.route('/getAllBusinessCenters',methods=['GET'])
-@cross_origin(supports_credentials=True)
+
+@app.route('/getAllBusinessCenters', methods=['GET'])
 def getBusinessCenters():
     try:
         return business_center.getBusinessCenters(driver)
     except(ValueError, KeyError, TypeError):
         print("Some error which you'll need to debug")
 
-@app.route('/getAllRoles',methods=['GET'])
-@cross_origin(supports_credentials=True)
+
+@app.route('/getAllRoles', methods=['GET'])
 def getRoles():
     try:
         return role.getRoles(driver)
     except(ValueError, KeyError, TypeError):
         print("Some error which you'll need to debug")
 
-@app.route('/getAllPeople',methods=['GET'])
-@cross_origin(supports_credentials=True)
+
+@app.route('/getAllPeople', methods=['GET'])
 def getPeople():
     try:
         return people.getPeople(driver)
     except(ValueError, KeyError, TypeError):
         print("Some error which you'll need to debug")
 
-@app.route('/getPersonWithRole/<id>',methods=['POST','GET'])
-@cross_origin(supports_credentials=True)
+
+@app.route('/getPersonWithRole/<id>', methods=['POST', 'GET'])
 def getPeopleWithRole(id):
     try:
         return people.getPeopleWithRole(driver, id)
     except(ValueError, KeyError, TypeError):
         print("Some error which you'll need to debug")
 
-@app.route('/getPersonWithBCenter/<id>',methods=['POST','GET'])
-@cross_origin(supports_credentials=True)
+
+@app.route('/getPersonWithBCenter/<id>', methods=['POST', 'GET'])
 def getPeopleWithBCenter(id):
     try:
         return people.getPeopleWithBCenter(driver, id)
     except(ValueError, KeyError, TypeError):
         print("Some error which you'll need to debug")
 
-@app.route('/getPersonWithRoleUnderBCenter/<id>',methods=['POST','GET'])
-@cross_origin(supports_credentials=True)
+
+@app.route('/getPersonWithRoleUnderBCenter/<id>', methods=['POST', 'GET'])
 def getPersonWithRoleUnderBCenter(id=None):
     try:
         role = request.args.get('role')
@@ -67,5 +65,5 @@ def getPersonWithRoleUnderBCenter(id=None):
         print("Some error which you'll need to debug")
 
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0', port = 8000, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
